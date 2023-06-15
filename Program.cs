90d8c2f77c850ec2550a4285022ba5093d1ce346
kam355 <kam355@txstate.edu>
2023-06-15 15:25:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] board = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+            string[,] boardRest = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+           
+            bool player1;
+            bool winner;
+            bool vaild;
+
+            do
+            {
+                Console.WriteLine("Welcome to TickTacToe");
+                gameFuctions.printTable(board);
+                bool player1turn = true;
+                int turns = 0;
+
+                while (true)
+                {
+                    if (player1turn == true)
+                    {
+                        player1 = true;
+                        Console.Write("Player 1 Enter a number:");
+                        string num = Console.ReadLine();
+                        vaild = gameFuctions.IsUsed(board, num);
+                        if (vaild == true)
+                        {
+                            gameFuctions.updateTable(board, num, player1);
+                            player1turn = false;
+                            turns++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Plese enter a vaild number!");
+                        }
+                    }
+                    else
+                    {
+                        player1 = false;
+                        Console.Write("Player 2 Enter a number:");
+                        string num = Console.ReadLine();
+                        vaild = gameFuctions.IsUsed(board, num);
+                        if (vaild == true)
+                        {
+                            gameFuctions.updateTable(board, num, player1);
+                            player1turn = true;
+                            turns++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("enter a vaild number!");
+                        }
+                    }
+
+                    winner = gameFuctions.Checker(board);
+
+                    if (winner == true)
+                    {
+                        if (player1 == true)
+                        {
+                            Console.WriteLine("Player 1 wins");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Player 2 wins");
+                        }
+                        board = gameFuctions.refill(board);
+                        break;
+                    }
+                    else
+                    {
+                        gameFuctions.printTable(board);
+
+                    }
+
+                    if (turns == 9 && winner == false)
+                    {
+                        Console.WriteLine("Draw Game Over");
+                        board = gameFuctions.refill(board);
+                        break;
+
+                    }
+                }
+
+            } while (true);
+        }
+    }
+}
---
+++ gameFucrtions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    public class gameFuctions
+    {
+        public static bool Checker(string[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                {
+                    return true;
+                }
+
+            }
+            for (int i = 0; i < board.GetLength(1); i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                {
+                    return true;
+                }
+            }
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return true;
+            }
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+       public  static void updateTable(string[,] board, string num,bool player1)
+        {
+            for(int i = 0;i < board.GetLength(0);i++)
+            {
+                for (int j = 0;j < board.GetLength(1); j++)
+                {
+                    if (board[i,j] == num)
+                    {
+                        if(player1 == true)
+                        {
+                            board[i, j] = "X";
+                        }
+                        else
+                        {
+                            board[i, j] = "O";
+                        }
+                    }
+                }
+            }
+        }
+       public static void printTable(string[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static bool IsUsed(string[,] board, string num)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == num)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+        public static string [,] refill(string[,] board)
+        {
+            int a = 0;
+
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    a += 1;
+                    board[i, j] = a.ToString();
+                    
+                }
+            }
+            return board;
+        }
+    }
+}
